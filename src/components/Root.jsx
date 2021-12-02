d19470edec14dc1cf139780e742da0dcfdd52a58
mrhrifat <mrhrifat383759@gmail.com>
2021-12-03 01:17:46 +0600

    Add Root Components

---
+++ src/components/Root.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Root extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Root
